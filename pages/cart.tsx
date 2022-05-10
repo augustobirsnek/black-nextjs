97c8b131ee39cabef8fdf1ea4a70a08019f727f0
Augusto Luba Birsnek <augustobirsnek@gmail.com>
2022-05-10 19:10:57 -0300

    Aplicação finalizada

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,18 +1,31 @@
-import { NextPage } from "next";
-import Head from "next/head";
+import { NextPage } from "next"
+import Head from "next/head"
+import { Container } from "reactstrap"
+import Header from "../src/components/Header"
+import CartTable from "../src/components/CartTable"
+import CartTotal from "../src/components/CartTotal"
 
 const Cart: NextPage = () => {
     return (
         <>
-        <Head>
-            <title>Carrinho</title>
-            <meta name="description" content="Meu carrinho de compras" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
+            <Head>
+                <title>Carrinho</title>
+                <meta name="description" content="Meu carrinho de compras" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-        <h1>
-            Carrinho
-        </h1>
+            <Header />
+
+            <main>
+                <Container className="mb-5">
+                <h1 className="my-5">
+                    Carrinho
+                </h1>
+
+                <CartTable />
+                <CartTotal />
+                </Container>
+            </main>
         </>
     )
 }
